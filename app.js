1430109c5df64ebfe59c4f0d762a2cc87cf507f4
CameronJWalker <cameron.jr.walker@icloud.com>
2022-06-06 19:05:41 -0700

    test commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 // map object
+const theMap = {
+    coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
+}
+
 
 // get coordinates via geolocation api
-
+async function coordinates(){
+    const pos = await new Promise((resolve, reject)=>{
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    });
+    return [pos.coords.latitude, pos.coords.longitude]
+}
 // get foursquare businesses
 
 // process foursquare array
@@ -11,3 +23,4 @@
 
 // business submit button
 
+//fsq36IKCED97kpj7hJ+MMVeFc/XDhf1ZpcwHq7WjIMYpg0Q= api key
